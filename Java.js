a694fa9096e27f522d70bf0e5f2f2f8ad8ad4011
EliTo01 <120269502+EliTo01@users.noreply.github.com>
2024-12-21 12:03:02 +0200

    Update Java.js

--- Java.js
+++ Java.js
@@ -143,7 +143,7 @@ ctx.clearRect(0, 0, messageCanvas.width, messageCanvas.height);
 // Dynamically adjust font size based on the message length
 const baseFontSize = 16; // Default font size
 const maxFontSize = 20;  // Maximum font size
-const minFontSize = 8;  // Minimum font size
+const minFontSize = 12;  // Minimum font size
 const fontAdjustment = Math.max(
   minFontSize,
   Math.min(baseFontSize, maxFontSize / Math.sqrt(randomMessage.length / 50))
@@ -153,7 +153,7 @@ ctx.fillStyle = '#444';  // Text color
 
 // Call the wrapText function to display the message
 const maxWidth = messageCanvas.width - 20; // Use 90% of the canvas width
-const lineHeight = 16;                      // Line height in pixels
+const lineHeight = 18;                      // Line height in pixels
 const x = 20;                               // Horizontal padding
 const y = 60;                               // Vertical start point
 
